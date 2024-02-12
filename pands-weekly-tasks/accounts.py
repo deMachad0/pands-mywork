71870e1925f6e426ff85980615a679899def49c7
Andre Queiroz Machado <de.machado90@gmail.com>
2024-02-12 11:03:16 +0000

    Third week graded tasks

--- pands-weekly-tasks/accounts.py
+++ pands-weekly-tasks/accounts.py
@@ -2,7 +2,7 @@
 # and outputs only the last 4 digits
 # Author: Andre Machado
 
-print("$ python accounts.py ")
+print("$ Python accounts.py ")
 number = (input("Please enter an 10 digit account number: "))
 
 result = number[-4:].rjust(len(number), 'X')
