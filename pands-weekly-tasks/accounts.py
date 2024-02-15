e2ebf9cd99aa199f373a811848d897d6af6c196e
Andre Queiroz Machado <de.machado90@gmail.com>
2024-02-15 23:58:22 +0000

    Weekly tasks graded

--- pands-weekly-tasks/accounts.py
+++ pands-weekly-tasks/accounts.py
@@ -3,7 +3,12 @@
 # Author: Andre Machado
 
 print("$ Python accounts.py ")
+
+# number will alocate the value of the user input
 number = (input("Please enter an 10 digit account number: "))
 
+# number[-4:] keeps the last 4 values 
+# rjust(len(number), 'X') adds X in all numbers of the left
+# len(number) return the length of the number
 result = number[-4:].rjust(len(number), 'X')
 print(result)
--- pands-weekly-tasks/bank.py
+++ pands-weekly-tasks/bank.py
@@ -4,10 +4,13 @@
 
 print("$ python bank.py ")
 
+# amount1 and amount2 will allocate the value of the user input
 amount1 = int( input("Enter the amount1(in cent): "))
 amount2 = int( input("Enter the amount2(in cent): "))
 
+# division to transform the value in cents
 result = (amount1 / 100 + amount2 / 100)
 
-
+# \N{euro sign} A character can be assigned to 
+# a string using its official Unicode name
 print(f"The sum of these is: \N{euro sign}{result}")
